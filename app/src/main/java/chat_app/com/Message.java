c16248f5c1859e87313cf5d0cbb0b30891fd07a1
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-01-22 12:10:55 +0300

    Update Message

--- app/src/main/java/chat_app/com/Message.java
+++ app/src/main/java/chat_app/com/Message.java
@@ -1,5 +1,18 @@
 package chat_app.com;
 
+import java.util.Date;
+
 public class Message {
-    
+    public String userName;
+    public String textMessage;
+    private long messageTime;
+
+    public Message() {}
+    public Message(String userName, String textMessage) {
+        this.userName = userName;
+        this.textMessage = textMessage;
+
+        this.messageTime = new Date().getTime();
+
+    }
 }
