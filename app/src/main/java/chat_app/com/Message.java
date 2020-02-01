2dce6ff129d2a32b3843bc461f1cad2f40a61d12
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-02-01 13:49:02 +0300

    Создали функции

--- app/src/main/java/chat_app/com/Message.java
+++ app/src/main/java/chat_app/com/Message.java
@@ -15,4 +15,28 @@ public class Message {
         this.messageTime = new Date().getTime();
 
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
 }
